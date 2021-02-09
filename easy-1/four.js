02db3c15607c0efb67bc1afae8efe582607de24f
Martina Ricci <martinaricci93@live.it>
2021-02-09 12:21:07 +1100

    exercise 4 completed

--- easy-1/four.js
+++ easy-1/four.js
@@ -2,13 +2,22 @@
 
 // Note: 1 square meter == 10.7639 square feet.
 
+// Modify the program so that it asks the user for the input type (meters or feet). Compute for the area accordingly, and log it and its conversion in parentheses.
+
 var readlineSync = require('readline-sync');
  
 // Wait for user's response.
 let roomLength = readlineSync.question('What is the length of your room? ');
 let roomWidth = readlineSync.question('And what is the width of your room? ');
+let inputType = readlineSync.question('Would you like to know the area in meters or feet? ');
 
 let squareMeters = roomLength * roomWidth;
 let squareFeet = squareMeters * 10.764;
 
-console.log(`Your room is: ${squareMeters.toFixed(2)} square meters or ${squareFeet.toFixed(2)} square feet`);
+if (inputType === 'meters') {
+    console.log(`Your room is: ${squareMeters.toFixed(2)} square meters`)
+} else {
+    console.log(`Your room is: ${squareFeet.toFixed(2)} square feet`)
+}
+
+// console.log(`Your room is: ${squareMeters.toFixed(2)} square meters or ${squareFeet.toFixed(2)} square feet`);
