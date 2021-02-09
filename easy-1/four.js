544a6734e0248975a42fd19ded83d619560bb287
Martina Ricci <martinaricci93@live.it>
2021-02-09 11:47:30 +1100

    installed readline-sync / exercise 3 and 4 completed

---
+++ easy-1/four.js
@@ -0,0 +1,14 @@
+// Build a program that asks the user to enter the length and width of a room in meters, and then logs the area of the room to the console in both square meters and square feet.
+
+// Note: 1 square meter == 10.7639 square feet.
+
+var readlineSync = require('readline-sync');
+ 
+// Wait for user's response.
+let roomLength = readlineSync.question('What is the length of your room? ');
+let roomWidth = readlineSync.question('And what is the width of your room? ');
+
+let squareMeters = roomLength * roomWidth;
+let squareFeet = squareMeters * 10.764;
+
+console.log(`Your room is: ${squareMeters} square meters or ${squareFeet} square feet`);
