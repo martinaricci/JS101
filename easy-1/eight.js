1b6001f8e63c81175ac8e66fc1816f11d504d853
Martina Ricci <martinaricci93@live.it>
2021-02-10 15:54:17 +1100

    edited ex 8, completed ex 9 and 10

--- easy-1/eight.js
+++ easy-1/eight.js
@@ -2,29 +2,27 @@
 
 // Assume this rule is valid for any year greater than year 0. Write a function that takes any year greater than 0 as input, and returns true if the year is a leap year, or false if it is not a leap year.
 
-function leapYear(year)
-{
+function leapYear(year) {
     // If divisible by 400 then return true
     // If divisible by 100 then return false
     // If divisible by 4 then return true
 
     // if (year % 400 ==0) {
-    //     console.log(year,true);
+    //     console.log(year, true);
     //     return true;
     // } else if (year % 100 == 0) {
-    //     console.log(year,false);
+    //     console.log(year, false);
     //     return false;
     // } else if (year % 4 == 0){
-    //     console.log(year,true);
+    //     console.log(year, true);
     //     return true;
     // } else {
-    //     console.log(year,false);
+    //     console.log(year,f alse);
     //     return false;
     // }
 
-    var resp = (year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
-    console.log(year,resp);
-    return resp;
+    let res = (year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
+    console.log(year, res);
 }
 
 leapYear(2016);      // true
