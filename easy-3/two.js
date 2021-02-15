edc32ac9ebc5fc62ac52708da8c712a66b133215
Martina Ricci <martinaricci93@live.it>
2021-02-15 22:31:01 +1100

    part of the 'further exploration' completed

--- easy-3/two.js
+++ easy-3/two.js
@@ -1,19 +1,37 @@
 // Write a function that will take a short line of text, and write it to the console log within a box.
 
 
-logInBox = str => {
+// FURTHER EXPLORATION
+// Modify this function so that it truncates the message if it doesn't fit inside a maximum width provided as a second argument (the width is the width of the box itself). You may assume no maximum if the second argument is omitted.
+
+// For a challenging but fun exercise, try word wrapping messages that are too long to fit, so that they appear on multiple lines but are still contained within the box. This isn't an easy problem, but it's doable with basic JavaScript.
+
+logInBox = (str, width) => {                                
     let emptySpaces = '';
     let dashes = '';
-    let lineOfString = '| ' + str + ' |';
-    for (let i = 0; i < str.length; i++) {
-        emptySpaces += ' ';
-        dashes += '-'; 
+    if (str.length > width) {
+        str = str.substring(0, width);
+        console.log(str);
     }
+    let lineOfString = '| ' + str + ' |';
+
+    if (width !== undefined) {
+        for (let i = 0; i < width; i++) {
+            emptySpaces += ' ';
+            dashes += '-';
+        }
+    } else {
+        for (let i = 0; i < str.length; i++) {
+            emptySpaces += ' ';
+            dashes += '-';     
+        }
+    }
+
     console.log(str.length);
-    console.log(emptySpaces.length);
 
 
     console.log('+-' + dashes + '-+' + '\n' + '| ' + emptySpaces + ' |' + '\n' + lineOfString + '\n' + '| ' + emptySpaces + ' |' + '\n' + '+-' + dashes + '-+');
 }
 
-logInBox('To boldly go where no one has gone before.');
+logInBox('To boldly go where no one has gone before.', 20);
+
