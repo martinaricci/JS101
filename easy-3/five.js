b318bef9e36b8f8f554c125b935fa5aca8d0bfbd
Martina Ricci <martinaricci93@live.it>
2021-02-16 18:59:48 +1100

    ex 5 partialy done

---
+++ easy-3/five.js
@@ -0,0 +1,26 @@
+// Write a function that takes a positive integer, n, as an argument, and logs a right triangle whose sides each have n stars. The hypotenuse of the triangle (the diagonal side in the images below) should have one end at the lower-left of the triangle, and the other end at the upper-right.
+
+
+// INPUT = 5
+// 1. Log 5 rows of *
+
+let rowsCounter = 0;
+
+triangle = num => {
+    while (rowsCounter < num) {
+        for (let i = 0; i < num; i++) {
+            rows = '*'.repeat(rowsCounter + 1);
+        }
+        rowsCounter++;
+        console.log(rows);
+    }
+}
+
+
+triangle(5);
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
