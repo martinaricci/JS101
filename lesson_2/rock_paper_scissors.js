29d91d3518f162685c2a776efe49c0f35dc50f8d
Martina Ricci <martinaricci93@live.it>
2021-03-02 10:37:22 +1100

    bonus 2 completed

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -8,14 +8,44 @@ function prompt(message) {
 let answer = '';
 
 do {
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  prompt(`Choose one: 'r' for ${VALID_CHOICES[0]}, 'p' for ${VALID_CHOICES[1]}, 'sc' for ${VALID_CHOICES[2]}, 'l' for ${VALID_CHOICES[3]}, 's' for ${VALID_CHOICES[4]}`);
   let choice = readline.question();
   
-  while (!VALID_CHOICES.includes(choice)) {
-      prompt("That's not a valid chioce");
+  let initialOfChoicesInArr = [];
+  
+  for (let i = 0; i < VALID_CHOICES.length; i++) { 
+    if (VALID_CHOICES[i] === 'scissors') {
+      initialOfChoicesInArr.push(VALID_CHOICES[i].charAt(0) + VALID_CHOICES[i].charAt(1));
+    } else {
+      initialOfChoicesInArr.push(VALID_CHOICES[i].charAt(0));
+    }
+    console.log(initialOfChoicesInArr);
+  }
+
+  
+  while (!initialOfChoicesInArr.includes(choice)) {
+      prompt("That's not a valid choice");
       choice = readline.question();
   };
   
+  switch(choice) {
+    case 'r':
+      choice = 'rock';
+      break;
+    case 'p':
+      choice = 'paper';
+      break;
+    case 'sc':
+      choice = 'scissors';
+      break;
+    case 'l':
+      choice = 'lizard';
+      break;
+    case 's':
+      choice = 'spock';
+      break;
+  }
+
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
   
@@ -23,7 +53,8 @@ do {
   
   if ((choice === 'rock' && computerChoice === 'scissors') ||
       (choice === 'paper' && computerChoice === 'rock') ||
-      (choice === 'scissors' && computerChoice === 'paper') ||(choice === 'rock' && computerChoice === 'lizard') ||
+      (choice === 'scissors' && computerChoice === 'paper') ||
+      (choice === 'rock' && computerChoice === 'lizard') ||
       (choice === 'lizard' && computerChoice === 'spock')) {
     prompt('You win!');
   } else if ((choice === 'rock' && computerChoice === 'paper') ||
