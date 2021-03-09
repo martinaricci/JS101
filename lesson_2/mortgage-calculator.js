94b891835aaf95221acbed52ed57556685d0ef70
Martina Ricci <martinaricci93@live.it>
2021-03-09 14:33:08 +1100

    handling 0% interest rate

--- lesson_2/mortgage-calculator.js
+++ lesson_2/mortgage-calculator.js
@@ -1,48 +1,83 @@
 const readline = require('readline-sync');
 
-function isInvalidNumber(number) {
+let isInvalidNumber = (number) => {
   return number.toString().trim() === '' ||
          Number(number) < 0   ||
          Number.isNaN(Number(number));
 }
 
-function isNotPositiveInteger(number) {
+let isInvalidInterestrate = (number) => {
+  return number.toString().trim() === '' ||
+         Number(number) <= 0   ||
+         Number.isNaN(Number(number));
+}
+
+let isNotPositiveInteger = (number) => {
   return !Number.isInteger(number);
 }
 
-let answer = 'y';
+let convertStrToNum = (str) => {
+  str = Number(str);
+}
 
-while (answer === 'y') {
+let answer;
+
+do {
+
+  // =========================================================================
+  // LOAN AMOUNT
   let loanAmount = readline.question('How much do you want to borrow?: ');
   // if the user types $ sign in front of the amount remove it from the string 
   if (loanAmount[0] === '$') {
     loanAmount = loanAmount.substring(1);
   }
-  //  convert the string into a number
-  loanAmount = Number(loanAmount);
+
+  convertStrToNum(loanAmount);
   console.log(`$${loanAmount}`);
 
   while (isInvalidNumber(loanAmount)) {
-    console.log('Must be a valid positive number');
-    loanAmount = Number(readline.question('How much do you want to borrow?: '));
+    console.log('Must be a valid positive number and use numbers only');
+
+    loanAmount = readline.question('How much do you want to borrow?: ');
+    if (loanAmount[0] === '$') {
+      loanAmount = loanAmount.substring(1);
+    }
+
+    convertStrToNum(loanAmount);
+    console.log(`$${loanAmount}`);
   }
 
+
+  // =======================================================================
+  // APR
   let APR = readline.question('What is the Annual Percentage Rate?: ');
   // if the user types $ sign in front of the amount remove it from the string
   if (APR[APR.length - 1] === '%') {
     APR = APR.slice(0, -1);
   }
-  //  convert the string into a number
-  APR = Number(APR);
+  
+  convertStrToNum(APR);
   console.log(`${APR}%`);
   let APRInFranctional = APR / 100;
   console.log(APRInFranctional);
 
-  while (isInvalidNumber(APR)) {
+  while (isInvalidInterestrate(APR)) {
     console.log('Must be a valid positive number');
-    APR = Number(readline.question('What is the Annual Percentage Rate?: '));
+    APR = readline.question('What is the Annual Percentage Rate?: ');
+
+    if (APR[APR.length - 1] === '%') {
+      APR = APR.slice(0, -1);
+    }
+
+    convertStrToNum(APR);
+    APRInFranctional = APR / 100;
+    console.log(APRInFranctional);
+    console.log(`${APR}%`);
   }
 
+
+  // =====================================================================
+  // YEARS
   let years = Number(readline.question('What is the loan duration in years?: '));
   console.log(years);
 
@@ -57,6 +92,8 @@ while (answer === 'y') {
     console.log(years)
   }
 
+  // ====================================================================
+  // MONTHLY PAYMENT CALCULATION
   let months = years * 12;
   let monthlyInterestRate = APRInFranctional / 12;
   let monthlyPayment = loanAmount * (monthlyInterestRate / (1 - Math.pow((1 + monthlyInterestRate), (-months))));
@@ -66,12 +103,11 @@ while (answer === 'y') {
 
   console.log(`Your monthly payment will be: $${Number(monthlyPayment.toFixed(2))}`);
 
+  // ==================================================================
+  // DO YOU WANT TO KEEP GOING?
   answer = readline.question("Would you like to do another calculation? \nType 'y' for yes, 'n' for no: ");
-  
+
   if (answer === 'n') {
-    break;
-  } else if (answer !== 'y') {
-    console.log('Please give a valid answer');
-    answer = readline.question("Would you like to do another calculation? \nType 'y' for yes, 'n' for no: ");
+    break; 
   }
-}
+} while (answer === 'y');
