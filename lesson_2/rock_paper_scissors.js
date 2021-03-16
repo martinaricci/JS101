8fa61e9d8cfd880ca074beb5951822f9e4b4dce7
Martina Ricci <martinaricci93@live.it>
2021-03-16 16:27:29 +1100

    finalised game for review

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,12 +1,19 @@
 const readline = require('readline-sync');
 const CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 const VALID_CHOICES = ['r', 'p', 'sc', 'l', 's'];
+const WINNING_COMBOS = {
+  rock:     ['scissors', 'lizard'],
+  paper:    ['rock',     'spock'],
+  scissors: ['paper',    'lizard'],
+  lizard:   ['paper',    'spock'],
+  spock:    ['rock',     'scissors'],
+};
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-let answer = '';
+let answer = 'y';
 let yourWins = 0;
 let computerWins = 0;
 
@@ -18,26 +25,19 @@ let displayResults = (yourWins, computerWins) => {
   }
 };
 
+let playerWins = (choice, computerChoice) => {
+  return WINNING_COMBOS[choice].includes(computerChoice);
+};
+
 let displayWinners = (choice, computerChoice) => {
-  if ((choice === 'rock' && computerChoice === 'scissors') ||
-    (choice === 'paper' && computerChoice === 'rock') ||
-    (choice === 'scissors' && computerChoice === 'paper') ||
-    (choice === 'rock' && computerChoice === 'lizard') ||
-    (choice === 'lizard' && computerChoice === 'spock') ||
-    (choice === 'paper' && computerChoice === 'spock') ||
-    (choice === 'scissors' && computerChoice === 'lizard') ||
-    (choice === 'lizard' && computerChoice === 'paper')) {
+  if (playerWins(choice, computerChoice)) {
     prompt('---- You win! -----');
     yourWins += 1;
-  } else if ((choice === 'rock' && computerChoice === 'paper') ||
-  (choice === 'paper' && computerChoice === 'scissors') ||
-  (choice === 'scissors' && computerChoice === 'rock') ||
-  (choice === 'scissors' && computerChoice === 'spock') ||
-  (choice === 'spock' && computerChoice === 'lizard')) {
-    prompt('----- Computer wins! ------');
-    computerWins += 1;
+  } else if (choice === computerChoice) {
+    prompt('----- It\'s a tie ------');
   } else {
-    prompt("------ It's a tie -----");
+    prompt("------ Computer wins! -----");
+    computerWins += 1;
   }
 
   console.log(`Your wins: ${yourWins}`);
@@ -45,13 +45,25 @@ let displayWinners = (choice, computerChoice) => {
   displayResults(yourWins, computerWins);
 };
 
-while (!((yourWins === 5) || (computerWins === 5))) {
+let anotherGame = () => {
+  if (!((yourWins === 5) || (computerWins === 5))) {
+    while (answer !== 'n' && answer !== 'y') {
+      answer = readline.question("Would you like to do another calculation? \nType 'y' for yes, 'n' for no: ");
+      if (answer !== 'n' && answer !== 'y') {
+        console.log('Please give a valid answer.');
+      }
+    }
+  }
+  return answer === 'y';
+};
+
+do {
   prompt(`Choose one: '${VALID_CHOICES[0]}' for ${CHOICES[0]}, '${VALID_CHOICES[1]}' for ${CHOICES[1]}, '${VALID_CHOICES[2]}' for ${CHOICES[2]}, '${VALID_CHOICES[3]}' for ${CHOICES[3]}, '${VALID_CHOICES[4]}' for ${CHOICES[4]}`);
   let choice = readline.question();
 
   while (!VALID_CHOICES.includes(choice)) {
     prompt("That's not a valid choice");
-    choice = readline.question("Please re-enter your choice: ");
+    choice = readline.question('Please re-enter your choice: ');
   }
 
   if (VALID_CHOICES.includes(choice)) {
@@ -66,16 +78,6 @@ while (!((yourWins === 5) || (computerWins === 5))) {
   prompt(`You chose ${choice}, computer chose ${computerChoice}`);
   displayWinners(choice, computerChoice);
 
-  if (!((yourWins === 5) || (computerWins === 5))) {
-
-    prompt('Do you want to play again (y/n)?');
-    answer = readline.question().toLowerCase();
-    while (answer[0] !== 'n' && answer[0] !== 'y') {
-      prompt('Please enter "y" or "n".');
-      answer = readline.question().toLowerCase();
-    }
-    if (answer === 'n') {
-      break;
-    }
-  }
-}
+  answer = '';
+  anotherGame();
+} while (anotherGame());
