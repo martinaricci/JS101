1d3ea7f19a497bf2009c47d6a700d2bb2e3d6aa0
Martina Ricci <martinaricci93@live.it>
2021-03-18 11:19:03 +1100

    some of the medium 1 exercises completed

---
+++ lesson_3/medium-1.js
@@ -0,0 +1,26 @@
+// Write a program that outputs The Flintstones Rock! 10 times, with each line indented 1 space to the right of the line above it.
+let num = 0; 
+let asciiArt = (str) => {
+    while (num < 10) {
+        console.log(' '.repeat(num) + str);
+        num++;
+    }
+}
+
+asciiArt('The Flintstones Rock!');
+
+
+// ----------------
+// Starting with the string:
+let munstersDescription = "The Munsters are creepy and spooky.";
+// Return a new string that swaps the case of all of the letters:
+
+let newStr = '';
+let swapCase = (str) => {
+    for (let i = 0; i < str.length; i++) {
+        newStr += str[i].match(/[a-z]/) ? str[i].toUpperCase() : str[i].toLowerCase();
+    }
+    return newStr;
+}
+
+console.log(swapCase(munstersDescription));
