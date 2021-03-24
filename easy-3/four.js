9947b564b8e186f3a71de8ffc4e328901fc1797f
Martina Ricci <martinaricci93@live.it>
2021-03-24 13:14:36 +1100

    optimised fibonacci calculation on exerc 4

--- easy-3/four.js
+++ easy-3/four.js
@@ -1,16 +1,19 @@
 // Write a function that calculates and returns the index of the first Fibonacci number that has the number of digits specified by the argument. (The first Fibonacci number has an index of 1.)
+let fibonacci = (num, curr) => {
+    curr = curr || {};
 
-fibonacci = num => {
+    if (curr[num]) return curr[num];
     if (num <= 1) return 1;
-    
-    return fibonacci(num - 1) + fibonacci(num - 2);
+
+    return curr[num] = fibonacci(num - 1, curr) + fibonacci(num - 2, curr);
 }
 
-findFibonacciIndexByLength = length => {
+let findFibonacciIndexByLength = length => {
     let num = 0;
     let index = 1;
     let res;
     for (let i = 0; num >= 0; i++) {
+        // console.log(i);
         let fibonacciNum = fibonacci(num);
         fibonacciNumToStr = fibonacciNum.toString();
         if (fibonacciNum.toString().length === length) {
@@ -21,11 +24,8 @@ findFibonacciIndexByLength = length => {
         index++;
     }
     return index;
-
 }
 
 console.log(findFibonacciIndexByLength(2));       // 7
-
-// retest the below
-// console.log(findFibonacciIndexByLength(10));      // 45
-// console.log(findFibonacciIndexByLength(16));      // 74
+console.log(findFibonacciIndexByLength(10));      // 45
+console.log(findFibonacciIndexByLength(16));      // 74
