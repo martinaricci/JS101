7f1b2b9354141b69a16e2819f54931ab179ca395
Martina Ricci <martinaricci93@live.it>
2021-04-26 11:19:36 +1000

    added let and removed extra empty lines

--- exercises/easy-3/one.js
+++ exercises/easy-3/one.js
@@ -1,19 +1,19 @@
 // Write a function that takes a string argument and returns a new string that contains the value of the original string with all consecutive duplicate characters collapsed into a single character.
 
 
-crunch = str => {
-    let newStr = '';
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] !== str[i + 1]) {
-            newStr += str[i];
-        }
+let crunch = str => {
+  let newStr = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[i + 1]) {
+      newStr += str[i];
     }
-    return newStr;
-}
+  }
+  return newStr;
+};
 
 
-console.log(crunch('dddaaiillyy ddoouubbllee'));    // "daily double"
-console.log(crunch('4444abcabccba'));              // "4abcabcba"
-console.log(crunch('ggggggggggggggg'));            // "g"
-console.log(crunch('a'));                          // "a"
-console.log(crunch(''));                           // ""
+console.log(crunch('dddaaiillyy ddoouubbllee')); // "daily double"
+console.log(crunch('4444abcabccba')); // "4abcabcba"
+console.log(crunch('ggggggggggggggg')); // "g"
+console.log(crunch('a')); // "a"
+console.log(crunch('')); // ""
