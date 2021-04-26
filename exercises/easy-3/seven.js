f77aa340f7cf0420a15de3b94017252a97e9afe9
Martina Ricci <martinaricci93@live.it>
2021-04-26 12:16:50 +1000

    refactored and completed

--- exercises/easy-3/seven.js
+++ exercises/easy-3/seven.js
@@ -1,13 +1,22 @@
-// Write a function that returns the number provided as an argument multiplied by two, unless the argument is a double number; return double numbers as-is.
+// Double Doubles
+// --------------------
 
-twice = num => {
-    // console.log(num * 2);
-    let spliter = num.toString();
-    let half = spliter / 2;
-    console.log(spliter);
-    console.log(half);
+// input: number
+// output: number
+// make the number into strings of digits
+// find the middle index of the array and check the numbers before and after that middle index
+// if the numbers before it are equal to the numbers after it, return the same number
+// else multiply the number by 2
 
-    // if ()
+let twice = num => {
+    let numToStrings = num.toString().split('');
+    let firstHalf = '';
+    let secondHalf = '';
+    let middleIdx = numToStrings.length / 2;
+    for (let idx = 0; idx < numToStrings.length; idx++) {
+        firstHalf.length !== middleIdx ? firstHalf += numToStrings[idx] : secondHalf += numToStrings[idx];
+    }
+    firstHalf === secondHalf ? console.log(num) : console.log(num * 2);
 }
 
 twice(37);          // 74
